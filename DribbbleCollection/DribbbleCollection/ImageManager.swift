c42ae3c67f5a9e5b61f18729dc5fb66992c852b4
Serg T <sergtsaeb@gmail.com>
2017-06-07 00:02:08 -0700

    ready for Dribbble token authorization and matching endpoints

--- DribbbleCollection/DribbbleCollection/ImageManager.swift
+++ DribbbleCollection/DribbbleCollection/ImageManager.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 private let _singletonInstance = ImageManager()
 private let kLazyLoadMaxCacheImageSize = 20
@@ -28,6 +29,47 @@ class ImageManager: NSObject {
         return imageCache[url]
     }
     func clearCache() { imageCache.removeAll() }
+    
+    
+    func callAlamo(url: String) {
+        
+//                Alamofire.request("https://api.dribbble.com/v1/user?access_token=OAUTH_TOKEN")(DataResponse<Any>) -> Void)
+        
+//            Alamofire.request(url).responseJSON(completionHandler: {
+//                response in
+////                    if let value = response.result.value as? JSONStandard, let shot = value["shot"] as? JSONStandard
+//                    {
+//                        print(shot)
+//                    }
+//        
+//        
+//        
+//            })
+    }
+    
+    func downloadImageFromURL(_ urlString: String, completion: ((_ success: Bool, _ image: UIImage?) -> Void)?) {
+        // do we have this cached?
+        if let cachedImage = cachedImageForURL(url: urlString) {
+            DispatchQueue.main.async(execute: {completion?(true, cachedImage) })
+        } else if let url = URL(string: urlString) { // download from URL asynchronously
+            let session = URLSession.shared
+            let downloadTask = session.downloadTask(with: url, completionHandler: { (retrievedURL, response, error) -> Void in
+                var found = false
+                if error != nil { print("Error downloading image \(url.absoluteString): \(error!.localizedDescription)") }
+                else if retrievedURL != nil {
+                    if let data = try? Data(contentsOf: retrievedURL!) {
+                        if let image = UIImage(data: data) {
+                            found = true
+                            self.cacheImage(image: image, forURL: url.absoluteString)
+                            DispatchQueue.main.async(execute: { completion?(true, image) });
+                        }
+                    }
+                }
+                if !found { DispatchQueue.main.async(execute: { completion?(false, nil) }); }
+            })
+            downloadTask.resume()
+        } else { completion?(false, nil) }
+    }
     
     
 }
